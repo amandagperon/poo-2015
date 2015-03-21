871b6fe9b289e150d4e379820255afd5e55f8058
Amanda Peron <amandagperon@gmail.com>
2015-03-20 22:43:10 -0300

    Pratica 2

---
+++ poo-paint/testes/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using poo_paint;
+
+namespace testes
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        
+        [TestMethod]
+        public void Teste()
+        {
+            Retangulo.ZeraContador();
+            Assert.AreEqual(0, Retangulo.LeContador());
+            Retangulo r1 = new Retangulo(10, 20, 50, 60);
+            Retangulo r2 = new Retangulo(5, 5, 10, 200);
+            Assert.AreEqual(2, Retangulo.LeContador());
+            Retangulo r3 = new Retangulo(5, 5, 3, 67);
+            Assert.AreEqual(3, Retangulo.LeContador());
+        }
+    }
+}
